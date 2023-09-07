39efaf37858b47ee52df9e0a516b4466c8fff075
Mostafa Semnani <mostafa.semnani.74@gmail.com>
2023-09-07 17:33:40 +0330

    add PersonService aspect for implementing aop

---
+++ src/main/java/ir/mostafa/semnani/phonebook/aop/PersonServiceAspect.java
@@ -0,0 +1,24 @@
+package ir.mostafa.semnani.phonebook.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class PersonServiceAspect {
+    @Before("execution(* ir.mostafa.semnani.phonebook.model.service.PersonService.save(..))")
+    public void beforeSave(JoinPoint joinPoint) {
+        System.out.println("before save join point : " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @After("execution(* ir.mostafa.semnani.phonebook.model.service.PersonService.save(..))")
+    public void afterSave(JoinPoint joinPoint) {
+        System.out.println("after save join point : " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+}
