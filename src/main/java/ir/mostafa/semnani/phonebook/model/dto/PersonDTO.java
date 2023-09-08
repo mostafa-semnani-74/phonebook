33e8bfb1166553d094f1a3bd3cf309f31b42165e
Mostafa Semnani <mostafa.semnani.74@gmail.com>
2023-09-08 16:22:23 +0330

    add logging for person service

--- src/main/java/ir/mostafa/semnani/phonebook/model/dto/PersonDTO.java
+++ src/main/java/ir/mostafa/semnani/phonebook/model/dto/PersonDTO.java
@@ -2,9 +2,11 @@ package ir.mostafa.semnani.phonebook.model.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @Builder
+@ToString
 public class PersonDTO {
     private Long id;
     private String name;
--- src/main/java/ir/mostafa/semnani/phonebook/model/service/impl/PersonServiceImpl.java
+++ src/main/java/ir/mostafa/semnani/phonebook/model/service/impl/PersonServiceImpl.java
@@ -6,6 +6,7 @@ import ir.mostafa.semnani.phonebook.model.mapper.PersonMapper;
 import ir.mostafa.semnani.phonebook.model.repository.PersonRepository;
 import ir.mostafa.semnani.phonebook.model.service.PersonService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,31 +16,38 @@ import java.util.List;
 @Transactional
 @AllArgsConstructor
 @Service
+@Slf4j
 public class PersonServiceImpl implements PersonService {
     @Autowired
     private final PersonRepository personRepository;
 
     @Transactional(readOnly = true)
     public List<PersonDTO> findAll() {
-        return PersonMapper.toDTOs(personRepository.findAll());
+        List<Person> personList = personRepository.findAll();
+        log.info("{} persons found", personList.size());
+        return PersonMapper.toDTOs(personList);
     }
 
     @Transactional(readOnly = true)
     public PersonDTO findById(Long id) {
-        return PersonMapper.toDTO(personRepository.findById(id));
+        Person person = personRepository.findById(id);
+        log.info("person with id : {} found", person.getId());
+        return PersonMapper.toDTO(person);
     }
 
     public void save(PersonDTO personDTO) {
         personRepository.save(PersonMapper.toEntity(personDTO));
+        log.info("person saved : " + personDTO);
     }
 
     public PersonDTO update(PersonDTO personDTO) {
-        return PersonMapper.toDTO(
-                personRepository.update(PersonMapper.toEntity(personDTO))
-        );
+        PersonDTO savedPersonDTO = PersonMapper.toDTO(personRepository.update(PersonMapper.toEntity(personDTO)));
+        log.info("person updated : " + savedPersonDTO);
+        return savedPersonDTO;
     }
 
     public void delete(Long id) {
         personRepository.delete(id);
+        log.info("person with id : {} deleted", id);
     }
 }
