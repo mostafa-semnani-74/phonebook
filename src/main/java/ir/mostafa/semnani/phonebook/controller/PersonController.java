81b5ad1aa5a4af2e77246aa4e703ce0abd301442
Mostafa Semnani <mostafa.semnani.74@gmail.com>
2023-09-08 13:52:19 +0330

    add open api for documenting rest apis

--- src/main/java/ir/mostafa/semnani/phonebook/controller/PersonController.java
+++ src/main/java/ir/mostafa/semnani/phonebook/controller/PersonController.java
@@ -1,5 +1,12 @@
 package ir.mostafa.semnani.phonebook.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import ir.mostafa.semnani.phonebook.model.dto.PersonDTO;
 import ir.mostafa.semnani.phonebook.model.service.PersonService;
 import lombok.AllArgsConstructor;
@@ -13,32 +20,67 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/persons")
 @AllArgsConstructor
+@Tag(name = "Person", description = "Person APIs")
 public class PersonController {
     @Autowired
     private final PersonService personService;
 
+    @Operation(
+            summary = "Retrieve all persons",
+            description = "response is persons List")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found all the persons",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class)))})
+    })
     @GetMapping
     public ResponseEntity<List<PersonDTO>> findAll() {
         List<PersonDTO> persons = personService.findAll();
         return ResponseEntity.ok(persons);
     }
 
+    @Operation(summary = "Get a person by its id",
+            description = "response is a person")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "response is a person object",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PersonDTO.class)) })
+    })
     @GetMapping("/{id}")
     public ResponseEntity<PersonDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(personService.findById(id));
     }
 
+    @Operation(summary = "save a person",
+            description = "response is a saved person")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "response is a saved person object",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PersonDTO.class)) })
+    })
     @PostMapping
     public ResponseEntity<PersonDTO> save(@RequestBody PersonDTO personDTO) {
         personService.save(personDTO);
         return new ResponseEntity<>(personDTO, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "update a person",
+            description = "response is a saved person")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "response is a saved person object",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PersonDTO.class)) })
+    })
     @PutMapping
     public ResponseEntity<PersonDTO> update(@RequestBody PersonDTO personDTO) {
         return ResponseEntity.ok(personService.update(personDTO));
     }
 
+    @Operation(summary = "delete a person by its id",
+            description = "response is an ok 200 http status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "response is an ok 200 http status")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable Long id) {
         personService.delete(id);
