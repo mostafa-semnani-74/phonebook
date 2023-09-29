1fb14a0c0a0c7062291d8eb962c561327777314a
Mostafa Semnani <mostafa.semnani.74@gmail.com>
2023-09-29 09:36:29 +0330

    add PersonIntegrationTest findAll and save

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Java 11 \
 Spring Boot 2 \
 Spring Data JPA \
 Rest Api \
+Integration Test \
 PostgreSQL \
 Slf4j \
 Lombok
---
+++ src/test/java/ir/mostafa/semnani/phonebook/integrationtest/PersonIntegrationTest.java
@@ -0,0 +1,54 @@
+package ir.mostafa.semnani.phonebook.integrationtest;
+
+import ir.mostafa.semnani.phonebook.model.dto.PersonDTO;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class PersonIntegrationTest {
+    @LocalServerPort
+    private int port;
+    private static final String LOCAL_HOST_URL = "http://localhost:";
+    private static final String BASE_API_URL = "/api/v1/persons";
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    @Order(1)
+    public void savePersonIntegrationTest() {
+        String url = LOCAL_HOST_URL + port + BASE_API_URL;
+        PersonDTO personDTO = PersonDTO.builder()
+                .name("mosi")
+                .build();
+        ResponseEntity<PersonDTO> savedPersonDTO = testRestTemplate.postForEntity(url, personDTO, PersonDTO.class);
+        assertEquals(HttpStatus.CREATED, savedPersonDTO.getStatusCode());
+        assertNotNull(savedPersonDTO.getBody());
+        assertEquals("mosi", savedPersonDTO.getBody().getName());
+    }
+
+    @Test
+    @Order(2)
+    public void findAllPersonsIntegrationTest() {
+        String url = LOCAL_HOST_URL + port + BASE_API_URL;
+        ResponseEntity<PersonDTO[]> persons = testRestTemplate.getForEntity(url, PersonDTO[].class);
+        assertEquals(HttpStatus.OK, persons.getStatusCode());
+        assertNotNull(persons.getBody());
+
+        List<PersonDTO> personsDTOList = List.of(persons.getBody());
+        assertEquals(1, personsDTOList.size());
+        assertEquals("mosi", personsDTOList.get(0).getName());
+    }
+
+}
